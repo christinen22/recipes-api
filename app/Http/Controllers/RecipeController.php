ed41cdf1fac1cb15db74b057431bf154f25e906d
christinen22 <christine.elin.louise@gmail.com>
2023-07-24 18:11:43 +0200

    Trying different approach

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -78,19 +78,15 @@ class RecipeController extends Controller
                 $imagePath = 'storage/recipe_images/' . $fileName;
             }
 
-            // Convert ingredients to have actual line breaks (\n)
-
-            $ingredients = $request->input('ingredients');
-            $ingredientsWithLineBreaks = str_replace("\n* ", "\n", $ingredients);
-
             $recipe = Recipe::create([
                 'title' => $request->input('title'),
                 'category' => $request->input('category'),
                 'body' => $request->input('body'),
-                'ingredients' => $ingredientsWithLineBreaks, // Save ingredients with HTML <br> tags
+                'ingredients' => $request->input('ingredients'), // Save ingredients as is to try
                 'image' => $imagePath,
                 'category_id' => $request->input('category_id'),
             ]);
+
             // Retrieve the full image URL
             $imageUrl = $imagePath ? url(Storage::url($imagePath)) : null;
             // Return the response with the recipe and image URL
